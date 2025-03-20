503c0d0787adcd02a0b5c5480cfa1cea9778beca
= <=>
2025-03-20 08:55:55 +0200

    added middleware

--- app.js
+++ app.js
@@ -7,8 +7,22 @@ app.use(express.urlencoded({ extended: true }));
 const path = require('path'); // Helps with file paths
 app.use(express.static(path.join(__dirname, 'public')));
 const cors = require("cors");
+const { send } = require('process');
 const port = process.env.PORT || 5500;
 app.use(cors());
+
+// Middleware Test
+app.get('/link-to-middlware-test/:id?', (req, res) => {
+    if (!req.params.id) {
+        req.myError = { reason: 'Please add id to the request', isServer: false };
+    } else {
+        req.myError = { reason: 'Server down', isServer: true };
+    }
+
+    throw Error(); // Sending error to the middleware
+
+
+});
 
 //Home Page
 app.get('/home', (req, res) => {
@@ -38,25 +52,17 @@ app.post('/contact-us', (req, res) => {
     res.send(`You sent a param named firstname, with value = ${req.body}`);
 });
 
+
+// Middleware handler
+app.use(function (err, req, res, next) {
+    if (req.myError.isServer) {
+        res.status(500).send('Something went wrong!');
+    } else {
+        res.status(400).send(`${req.myError.reason}`);
+    }
+})
+
+
 app.listen(port, () => {
     console.log(`Server listening at http://localhost:${port}`);
 });
-
-
-
-// // Get payments parameters
-// app.get('/payments', async (req, res) => {
-//     const methodPay = req.query.methodPay; // Get method from query parameter
-
-//     if (!methodPay) {
-//         return res.status(400).send({ error: 'methodPay is required query parameters.' });
-//     }
-
-//     try {
-//         const payments = await myRepository.getPayMethod(methodPay);
-//         res.json(payments);
-//     } catch (error) {
-//         console.error("Error:", error);
-//         res.status(500).send({ error: 'Failed to fetch order counts.' });
-//     }
-// });
